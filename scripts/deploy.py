8cc1c8a88048f7c302dfd9aae28b0509d1313913
jjreedv <87283488+jjreedv@users.noreply.github.com>
2021-09-30 11:52:37 -1000

    Backend v > 1

---
+++ scripts/deploy.py
@@ -0,0 +1,49 @@
+from brownie import accounts, network, config, ChenToken, TokenFarm
+from web3 import Web3
+
+LOCAL_BLOCKCHAIN_ENVIROMENTS = ["hardhat",
+                                "development", "ganache", "mainnet-fork"]
+
+KEPT_TOKENS = Web3.toWei(100, "ether")
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+        return accounts[0]
+    if id:
+        return accounts.load[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+def add_allowed_tokens(farm, dict_of_allowed_tokens, account):
+    for token in dict_of_allowed_tokens:
+        add_tx = farm.addAllowedTokens(token.address, {"from": account})
+        add_tx.wait(1)
+        set_tx = farm.setPriceFeedContract(
+            token.address, dict_of_allowed_tokens[token], {"from": account})
+        set_tx.wait(1)
+        return farm
+
+
+def deploy_token_farm_and_chen_token():
+    account = get_account()
+    chen = ChenToken.deploy({"from": account})
+    farm = TokenFarm.deploy(chen.address, {"from": account})
+    # tx = chen.transfer(farm.address, chen.totalSupply() -
+    #                   KEPT_TOKENS, {"from": account})
+    # tx.wait(1)
+    weth_token = config["networks"]["rinkeby"]["weth_token"]
+    fau_token = config["networks"]["rinkeby"]["fau_token"]
+    dict_of_allowed_tokens = {
+        chen: config["networks"]["rinkeby"]["dai_usd_price_feed"],
+        fau_token: config["networks"]["rinkeby"]["dai_usd_price_feed"],
+        weth_token: config["networks"]["rinkeby"]["eth_usd_price_feed"]
+    }
+    add_allowed_tokens(farm, dict_of_allowed_tokens, account)
+    return farm, chen
+
+
+def main():
+    deploy_token_farm_and_chen_token()
